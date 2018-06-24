a00ca8c75199d158a4eee6c73f8849c80b8a6afd
zhangguobiao <zhangguobiao@huxiu.com>
2018-06-24 08:54:20 +0800

    koa-blog

---
+++ config/default.js
@@ -0,0 +1,13 @@
+const config = {
+    //启动端口
+    port: 3003,
+    database: {
+        DATABASE: "nodesql",
+        HOST: "localhost",
+        USERNAME: "root",
+        PASSWORD: "root",
+        port: "3306"
+    }
+}
+
+module.exports = config;
