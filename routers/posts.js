4284330946d5a0ddbe6511231401da62b1291be1
zhangguobiao <zhangguobiao@huxiu.com>
2018-06-25 17:45:22 +0800

    koa-blog 后端接口文件的修改

--- routers/posts.js
+++ routers/posts.js
@@ -1,67 +1,66 @@
-var router=require('koa-router')();
+var router = require('koa-router')();
 // 处理数据库（之前已经写好，在mysql.js）
-var userModel=require('../lib/mysql.js');
+var userModel = require('../lib/mysql.js');
 // 时间中间件
-var moment=require('moment');
+var moment = require('moment');
 
 // get '/'页面
 router.get('/',async (ctx,next)=>{
     ctx.redirect('/posts');
 });
+
 // get '/posts'页面
 router.get('/posts',async (ctx,next)=>{
-
     // 这里我们先通过查找有没有类似/posts?author=XXX 的连接跳转，如果有就执行下面这句话，把用户名取下来，由于用户名存在中文，所以我们进行解码
     if (ctx.request.querystring) {
-    console.log('ctx.request.querystring',decodeURIComponent(ctx.request.querystring.split('=')[1]))
-    await userModel.findDataByUser(decodeURIComponent(ctx.request.querystring.split('=')[1]))
-        .then(result=>{
-        res=JSON.parse(JSON.stringify(result))
-        console.log(res)
-});
-    await ctx.render('posts',{
-        session:ctx.session,
-        posts:res
-    });
-}else{
-    // 如果连接是正常的 如 /posts 则我们获取的是全部文章的列表，所以通过findAllPost查找全部文章并向模板传递数据posts， posts的值为res
-    await userModel.findAllPost()
-        .then(result=>{
-        console.log(result);
-    res=JSON.parse(JSON.stringify(result));
-    console.log('post',res);
-});
-    await ctx.render('posts',{
-        session:ctx.session,
-        posts:res
-    });
-}
+        console.log('ctx.request.querystring',decodeURIComponent(ctx.request.querystring.split('=')[1]));
+        await userModel.findDataByUser(decodeURIComponent(ctx.request.querystring.split('=')[1]))
+            .then(result=>{
+                res=JSON.parse(JSON.stringify(result));
+                console.log(res);
+            });
+        await ctx.render('posts',{
+            session:ctx.session,
+            posts:res
+        });
+    }else{
+        // 如果连接是正常的 如 /posts 则我们获取的是全部文章的列表，所以通过findAllPost查找全部文章并向模板传递数据posts， posts的值为res
+        await userModel.findAllPost()
+            .then(result=>{
+                console.log(result);
+                res=JSON.parse(JSON.stringify(result));
+                console.log('post',res);
+            });
+        await ctx.render('posts',{
+            session:ctx.session,
+            posts:res
+        });
+    }
 });
 
 // get '/create'
 router.get('/create',async (ctx,next)=>{
     await ctx.render('create',{
-        session:ctx.session,
+        session:ctx.session
     });
 });
 
 // psot '/create'
 router.post('/create',async (ctx,next)=>{
-
     console.log(ctx.session.user);
-var title=ctx.request.body.title;
-var content=ctx.request.body.content;
-var id=ctx.session.id;
-var name=ctx.session.user;
-var time=moment().format('YYYY-MM-DD HH:mm');
-console.log([name,title,content,id,time]);
-// 这里我们向数据库插入用户名、标题、内容、发表文章用户的id、时间，成功返回true，失败为false
-await userModel.insertPost([name,title,content,id,time])
-    .then(()=>{
-    ctx.body='true';
-}).catch(()=>{
-    ctx.body='false';
-});
+    var title=ctx.request.body.title;
+    var content=ctx.request.body.content;
+    var id=ctx.session.id;
+    var name=ctx.session.user;
+    var time=moment().format('YYYY-MM-DD HH:mm');
+    console.log([name,title,content,id,time]);
+    // 这里我们向数据库插入用户名、标题、内容、发表文章用户的id、时间，成功返回true，失败为false
+    await userModel.insertPost([name,title,content,id,time])
+        .then(()=>{
+            ctx.body='true';
+        }).catch(()=>{
+            ctx.body='false';
+        });
 });
 
 //单篇文章
@@ -70,24 +69,31 @@ router.get('/posts/:postId',async (ctx,next)=>{
     // 通过文章id查找返回数据，然后增加pv 浏览 +1
     await userModel.findDataById(ctx.params.postId)
         .then(result=>{
-        res=JSON.parse(JSON.stringify(result));
-        res_pv=parseInt(JSON.parse(JSON.stringify(result))[0]['pv']);
-        res_pv+=1;
-    console.log(res);
+            res=JSON.parse(JSON.stringify(result));
+            res_pv=parseInt(JSON.parse(JSON.stringify(result))[0]['pv']);
+            res_pv+=1;
+            console.log(res);
+        });
+
+    //通过id找评论，然后把数据返回给前端
+    await userModel.findCommentById(ctx.params.postId)
+        .then(result => {
+            comment_res = result
+        });
+
+    // 渲染模板，并传递三个数据
+    await ctx.render('sPost',{
+        session:ctx.session,
+        posts:res,
+        comments: comment_res
+    });
 });
 
-// 渲染模板，并传递三个数据
-await ctx.render('sPost',{
-    session:ctx.session,
-    posts:res
-})
-});
-
-//发表评论
+//发表评论接口
 router.post('/:postId',async (ctx,next)=>{
-    var name=ctx.session.user;
-    var content=ctx.request.body.content;
-    var postId=ctx.params.postId;
+    var name = ctx.session.user;
+    var content = ctx.request.body.content;
+    var postId = ctx.params.postId;
 
     // 插入评论的用户名，内容和文章id
     await userModel.insertComment([name,content,postId]);
@@ -100,84 +106,85 @@ router.post('/:postId',async (ctx,next)=>{
     // 更新评论数 res_comments
     await userModel.updatePostComment([res_comments,postId])
         .then(()=>{
-        ctx.body='true';
-    }).catch(()=>{
-        ctx.body='false';
-    });
+            ctx.body='true';
+        }).catch(()=>{
+            ctx.body='false';
+        });
 });
 
-//删除评论
+//删除评论接口
 router.get('/posts/:postId/comment/:commentId/remove',async (ctx,next)=>{
 
-    var postId=ctx.params.postId
-    var commentId=ctx.params.commentId
+    var postId = ctx.params.postId;
+    var commentId = ctx.params.commentId;
     await userModel.findDataById(postId)
         .then(result=>{
-        res_comments=parseInt(JSON.parse(JSON.stringify(result))[0]['comments'])
-        console.log('res',res_comments)
-    res_comments-=1
-console.log(res_comments)
-})
-await userModel.updatePostComment([res_comments,postId])
-await userModel.deleteComment(commentId)
-    .then(()=>{
-    ctx.body={
-    data:1
-}
-}).catch(()=>{
-    ctx.body={
-    data:2
-}
-})
+            res_comments=parseInt(JSON.parse(JSON.stringify(result))[0]['comments']);
+            console.log('res',res_comments);
+            res_comments-=1;
+            console.log(res_comments);
+        });
+    await userModel.updatePostComment([res_comments,postId]);
+    await userModel.deleteComment(commentId)
+        .then(()=>{
+            ctx.body={
+                data:1
+            }
+        }).catch(()=>{
+            ctx.body={
+                data:2
+            }
+        });
 });
 
-//删除文章
+//删除文章接口 (所有接口的操作，最终都会落实到数据库里面)
 router.get('/posts/:postId/remove',async (ctx,next)=>{
-
     var postId=ctx.params.postId;
 
     await userModel.deleteAllPostComment(postId);
     await userModel.deletePost(postId)
         .then(()=>{
-        ctx.body={
-        data:1
-    }
-}).catch(()=>{
-    ctx.body={
-    data:2
-}
+            ctx.body={
+                //这里是返回给前端的数据格式
+                data:1
+            }
+        }).catch(()=>{
+            ctx.body={
+                data:2
+            }
+        });
 });
 
-});
-
-// get '/posts/:postId/edit'
+// get '/posts/:postId/edit'  访问编辑文章页面
 router.get('/posts/:postId/edit',async (ctx,next)=>{
     var name=ctx.session.user;
     var postId=ctx.params.postId;
 
     await userModel.findDataById(postId)
         .then(result=>{
-        res=JSON.parse(JSON.stringify(result));
-        console.log('修改文章',res);
+            res=JSON.parse(JSON.stringify(result));
+            console.log('修改文章',res);
+        });
+
+    await ctx.render('edit',{
+        session:ctx.session,
+        posts:res
+    });
 });
-await ctx.render('edit',{
-    session:ctx.session,
-    posts:res
-});
-});
-// post '/posts/:postId/edit'
+
+// post '/posts/:postId/edit' 编辑文章页面，内容提交 接口
 router.post('/posts/:postId/edit',async (ctx,next)=>{
-    var title=ctx.request.body.title;
-    var content=ctx.request.body.content;
-    var id=ctx.session.id;
-    var postId=ctx.params.postId;
+    var title = ctx.request.body.title;
+    var content = ctx.request.body.content;
+    var id = ctx.session.id;
+    var postId = ctx.params.postId;
 
     await userModel.updatePost([title,content,postId])
         .then(()=>{
-        ctx.body='true';
-}).catch(()=>{
-    ctx.body='false';
-});
+            ctx.body='true';
+        }).catch(()=>{
+            ctx.body='false';
+        });
 });
 
-module.exports=router;
+module.exports = router;
